90fce18cae6615c22b3403fb35508e5096f62a0b
Angel Gabriel Alvarado Hernandez <alvaradohernandezangel5@gmail.com>
2026-01-27 12:38:51 -0600

    Carpetas secuenciales y Condicionales

---
+++ Condicionales/condicionales.py
@@ -0,0 +1,33 @@
+# Condicionales cob python
+
+# if, else, elif
+
+"""
+if exp_bool:
+	instrucciones
+
+if exp_bool:
+	instrucciones
+else:
+	instrucciones
+
+if exp_bool:
+	instrucciones
+elif expo_bool:
+	instrucciones
+else:
+	instrucciones					
+"""
+
+print('Inicio')
+
+numero = int(input('Ingresa un número: '))
+
+if numero > 0:
+	print('Es un número Positivo')
+elif numero == 0:
+	print('Es cero')
+else:
+	print('No es numero Positivo')	
+
+	print('Fin')
