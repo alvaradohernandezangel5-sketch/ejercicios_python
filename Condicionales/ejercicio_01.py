debcf29fba3e1322d101b72e6889cd6865de53fb
Angel Gabriel Alvarado Hernandez <alvaradohernandezangel5@gmail.com>
2026-01-28 16:23:05 -0600

    ejercicios condicionales parte 1

---
+++ Condicionales/ejercicio_01.py
@@ -0,0 +1,10 @@
+# Pedir dos números y decir que si el primero es mayor
+
+num_1 = int(input('Ingresa el primer número: '))
+num_2 = int(input('Ingresa el segundo número: '))
+
+
+if num_1>num_2:
+	print("El primer numero es mayor")
+else:
+	print("El segundo numero es mayor")
---
+++ Condicionales/ejercicio_03.py
@@ -0,0 +1,8 @@
+# Escribe un programa que lea un número e indique si es par o impar.
+
+numero = int(input(" Ingresa un número: "))
+
+if numero % 2 == 0: 
+	print("El numero que ingresaste es par")
+else:
+	print("El numero que ingresaste es impar")
